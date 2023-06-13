a5ec58efdba259e87f80ba6d2730059ebdd0c892
DaffaAdityaDev <daffaaditya990@gmail.com>
2023-06-14 01:43:19 +0700

    update api key

--- app/src/main/java/com/example/recyclops/ui/utils/Utils.kt
+++ app/src/main/java/com/example/recyclops/ui/utils/Utils.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
+import android.media.ExifInterface
 import android.net.Uri
 import android.os.Environment
 import java.io.*
@@ -26,9 +27,17 @@ fun createCustomTempFile(context: Context): File {
 }
 
 fun rotateFile(file: File, isBackCamera: Boolean = false) {
+    val exif = ExifInterface(file.path)
+    val orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)
+    val rotation: Float = when (orientation) {
+        ExifInterface.ORIENTATION_ROTATE_90 -> 90f
+        ExifInterface.ORIENTATION_ROTATE_180 -> 180f
+        ExifInterface.ORIENTATION_ROTATE_270 -> 270f
+        else -> 0f
+    }
+
     val matrix = Matrix()
     val bitmap = BitmapFactory.decodeFile(file.path)
-    val rotation = if (isBackCamera) 90f else -90f
     matrix.postRotate(rotation)
     if (!isBackCamera) {
         matrix.postScale(-1f, 1f, bitmap.width / 2f, bitmap.height / 2f)
